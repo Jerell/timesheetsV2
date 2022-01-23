3b23bd4d6cf4a7508eec700071552eed53629a7c
jerell <jerell1998@gmail.com>
2022-01-23 23:39:27 +0000

    openapi

--- src/task/dto/newTask.dto.ts
+++ src/task/dto/newTask.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IDay } from 'src/common/daynum';
 
 export class newTaskDTO {
   readonly id: string;
+  @ApiProperty({
+    description: 'The first day of the task',
+    example: '2022-01-23',
+    type: 'string',
+  })
   readonly start: IDay;
+  @ApiProperty({
+    description: 'The first day of the task',
+    example: '2022-01-23',
+    type: 'string',
+  })
   readonly end: IDay;
 }
